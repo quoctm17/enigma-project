e30061393c36cf14cc6457ada6b4234fafcfcdcf
quoctm17 <tranminhquoc0711@gmail.com>
2024-04-16 20:14:02 +0700

    UI for Header Text

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -13,27 +13,27 @@ function Header() {
       <nav aria-label="Global" className="hidden md:block">
         <ul className="flex items-center gap-6 text-sm">
           <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> About </a>
+            <a className="text-white-500 transition hover:text-gray-500/75" href="#"> About </a>
           </li>
 
           <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Careers </a>
+            <a className="text-white-500 transition hover:text-gray-500/75" href="#"> Careers </a>
           </li>
 
           <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> History </a>
+            <a className="text-white-500 transition hover:text-gray-500/75" href="#"> History </a>
           </li>
 
           <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Services </a>
+            <a className="text-white-500 transition hover:text-gray-500/75" href="#"> Services </a>
           </li>
 
           <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Projects </a>
+            <a className="text-white-500 transition hover:text-gray-500/75" href="#"> Projects </a>
           </li>
 
           <li>
-            <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Blog </a>
+            <a className="text-white-500 transition hover:text-gray-500/75" href="#"> Blog </a>
           </li>
         </ul>
       </nav>
