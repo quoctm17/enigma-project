8b4b3abc418b1ee34c8e68680fde0e569d7a5d89
quoctm17 <tranminhquoc0711@gmail.com>
2024-05-05 14:46:50 +0700

    Add Workspace Layout

--- app/(routes)/workspace/[fileId]/page.tsx
+++ app/(routes)/workspace/[fileId]/page.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import WorkspaceHeader from '../_components/WorkspaceHeader'
+import Editor from '../_components/Editor'
 
 function Workspace() {
     return (
         <div>
-            Workspace
+            <WorkspaceHeader />
+
+            {/* Workspace Layout */}
+            <div className='grid grid-cols-1
+            md:grid-cols-2'>
+                {/* Document  */}
+                <div className='h-screen'>
+                    <Editor />
+                </div>
+                {/* Whiteboard/canvas */}
+                <div className='bg-red-400 h-screen'>
+                    Canvas
+                </div>
+            </div>
         </div>
     )
 }
