81e298f30847e9327760be528723e798ee25704b
quoctm17 <tranminhquoc0711@gmail.com>
2024-05-05 13:24:53 +0700

    Add Management File Page Update Total File Progress Add Dashboard Function in File Management

--- app/(routes)/dashboard/layout.tsx
+++ app/(routes)/dashboard/layout.tsx
@@ -3,7 +3,9 @@ import { api } from '@/convex/_generated/api';
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
 import { useConvex } from 'convex/react';
 import { useRouter } from 'next/navigation';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import SideNav from './_components/SideNav';
+import { FileListContext } from '@/app/_context/FilesListContext';
 
 function DashboardLayout(
     {
@@ -14,7 +16,9 @@ function DashboardLayout(
 ) {
     const convex = useConvex();
     const { user }: any = useKindeBrowserClient();
+    const [fileList_, setFileList_] = useState();
     const router = useRouter();
+
     useEffect(() => {
         user && checkTeam();
     }, [user])
@@ -29,9 +33,20 @@ function DashboardLayout(
     }
     return (
         <div>
-            {children}
+            <FileListContext.Provider value={{ fileList_, setFileList_ }}>
+                <div className='grid grid-cols-4'>
+                    <div className='h-screen w-64 fixed'>
+                        <SideNav />
+                    </div>
+                    <div className='col-span-4 ml-72'>
+                        {children}
+                    </div>
+                </div>
+            </FileListContext.Provider>
+
         </div>
     )
 }
 
 export default DashboardLayout
+
