2f57e56e5dcbe8670efea3821b42718383c6bdf6
quoctm17 <tranminhquoc0711@gmail.com>
2024-05-05 15:24:21 +0700

    Add Save Document to Convex Files Table

--- app/(routes)/workspace/_components/WorkspaceHeader.tsx
+++ app/(routes)/workspace/_components/WorkspaceHeader.tsx
@@ -1,9 +1,9 @@
 import { Button } from '@/components/ui/button'
-import { Link } from 'lucide-react'
+import { Link, Save } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
-function WorkspaceHeader() {
+function WorkspaceHeader({ onSave }: any) {
     return (
         <div className='p-3 border-b flex justify-between items-center'>
             <div className='flex gap-2 items-center'>
@@ -13,9 +13,16 @@ function WorkspaceHeader() {
                     width={40} />
                 <h2>File Name</h2>
             </div>
-            <Button className='h-8 text-[12px]
+            <div className='flex items-center gap-4'>
+                <Button className='h-8 text-[12px]
+            gap-2 bg-yellow-500 hover:bg-yellow-600'
+                    onClick={() => onSave()}
+                >
+                    Save <Save className='h-4 w-4' /></Button>
+                <Button className='h-8 text-[12px]
             gap-2 bg-blue-600 hover:bg-blue-700'>
-                Share <Link className='h-4 w-4' /></Button>
+                    Share <Link className='h-4 w-4' /></Button>
+            </div>
         </div>
     )
 }
