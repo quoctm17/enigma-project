99fa90f85c049df3b448e2bb5108e1eeada21253
quoctm17 <tranminhquoc0711@gmail.com>
2024-05-20 18:25:41 +0700

    Add blur Background for Header in HomePage & Animation when Scroll Down Screen

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,17 +1,33 @@
 import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs'
 import { ArrowRight } from 'lucide-react'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header className="absolute top-0 left-0 w-full text-white z-50">
+        <header className={`header-bg fixed top-0 left-0 w-full text-white z-50 transition-opacity duration-500 bg-black ${isScrolled ? 'bg-opacity-100' : 'bg-opacity-50'}`}>
             <div className="mx-auto flex h-16 max-w-screen-xl items-center px-4 sm:px-6 lg:px-8">
                 <a href='/' className='hover:text-gray-100/75'>
                     <div className='flex items-center gap-3 pl-4'>
-                        <Image src='/logo.png' alt='logo'
-                            width={50}
-                            height={50} />
+                        <Image src='/logo.png' alt='logo' width={50} height={50} />
                         <div className="text-white font-bold transition text-2xl"> Enigma </div>
                     </div>
                 </a>
@@ -19,19 +35,15 @@ function Header() {
                 <div className="flex flex-1 items-center pl-8 gap-8 justify-end md:justify-between">
                     <nav aria-label="Global" className="hidden md:block">
                         <ul className="flex items-center gap-6 text-sm">
-
                             <li>
                                 <a className="text-white transition hover:text-gray-100/75" href="#"> Use Cases </a>
                             </li>
-
                             <li>
                                 <a className="text-white transition hover:text-gray-100/75" href='/about'> About </a>
                             </li>
-
                             <li>
                                 <a className="text-white transition hover:text-gray-100/75" href="/pricing"> Pricing </a>
                             </li>
-
                             <li>
                                 <a className="text-white transition hover:text-gray-100/75" href="#"> Resources </a>
                             </li>
@@ -40,35 +52,18 @@ function Header() {
 
                     <div className="flex items-center gap-4">
                         <div className="sm:flex sm:gap-4">
-                            <span
-                                className="block rounded-md px-5 py-2.5 text-sm font-medium 
-                                text-white transition"
-                            >
+                            <span className="block rounded-md px-5 py-2.5 text-sm font-medium text-white transition">
                                 <LoginLink postLoginRedirectURL='/dashboard'>Login</LoginLink>
                             </span>
 
-                            <span
-                                className="hidden rounded-md bg-gray-100 
-                                px-4 py-2.5 text-sm font-medium
-                                text-black transition
-                                hover:text-slate-200 sm:block"
-                            >
+                            <span className="hidden rounded-md bg-gray-100 px-4 py-2.5 text-sm font-medium text-black transition hover:text-slate-200 sm:block">
                                 <RegisterLink className='flex items-center'>Register <ArrowRight className='ml-3' /></RegisterLink>
                             </span>
                         </div>
 
-                        <button
-                            className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
-                        >
+                        <button className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
                             <span className="sr-only">Toggle menu</span>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                            >
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
                         </button>
@@ -76,7 +71,7 @@ function Header() {
                 </div>
             </div>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
