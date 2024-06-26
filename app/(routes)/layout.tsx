7e91aaa6304f2fd29855f9c025483d3633ac9c9d
Trung Nguyen <trung@intra.co.kr>
2024-06-26 14:45:26 +0700

    Fix: suspense component error when build

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from 'react';
+import { Suspense } from 'react';
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
-  return (
-    <main>
-      {children}
-    </main>
-  );
+    return (
+        <main>
+            {/* // You could have a loading skeleton as the `fallback` too */}
+            <Suspense>{children}</Suspense>
+        </main>
+    );
 };
 
 export default RootLayout;
